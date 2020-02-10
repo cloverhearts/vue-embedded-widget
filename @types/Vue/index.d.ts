a09d025dc6510f880f97d1b56bf50d4339370485
CloverHearts <cloveerheartsdev+github@gmail.com>
2020-02-10 20:06:09 +0900

    Get props

--- @types/Vue/index.d.ts
+++ @types/Vue/index.d.ts
@@ -1,4 +1,13 @@
 declare namespace Vue {
+    enum PropsTypes {
+        String = 'String',
+        Number = 'Number',
+        Boolean = 'Boolean',
+        Array = 'Array',
+        Object = 'Object',
+        Function = 'Function',
+        Promise = 'Promise'
+    }
     interface Component {
         name:string,
         props:Object
