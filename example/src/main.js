de220f40595f9abf34983910c903133c86033271
CloverHearts <cloverheartsdev+github@gmail.com>
2020-04-01 22:44:14 +0900

    JUST PUSH

--- example/src/main.js
+++ example/src/main.js
@@ -2,13 +2,8 @@ import Vue from 'vue'
 import store from './store'
 import VEW from 'vue-embedded-widget'
 import HelloWorld from './components/HelloWorld'
-
+import App from './App'
 VEW(Vue)
-
-new Vue({
-  store,
-  render: h => h(App)
-}).$mount('#app')
 
 VEW.registerComponent(HelloWorld, "test-my-comp");
 VEW.registerComponent(HelloWorld, '*[data-vue-app="nice"]');
